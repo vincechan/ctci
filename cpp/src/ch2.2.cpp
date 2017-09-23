92e17fb4223bdb8060e766d8728c324887e91de5
vince <vince.yp.chan@gmail.com>
2017-09-23 03:08:31 -0700

    feat: ch2.3 ch2.4 solutions

--- cpp/src/ch2.2.cpp
+++ cpp/src/ch2.2.cpp
@@ -6,11 +6,31 @@
  * Find k th to the last element. Return -1 if it doesn't exist.
  */
 int findKthToLast(const LinkedList& list, int k) {
-  // TODO: implement
-  return 0;
+  // find total number of elements in the linked list
+  int count = 0;
+  Node* p = list.head;
+  while (p != nullptr) {
+    count++;
+    p = p->next;
+  }
+
+  // determine number of move next to get to kth to last element
+  p = list.head;
+  int moves = count - k - 1;
+  if (moves < 0) {
+    // kth element does not exist
+    return -1;
+  }
+
+  // go to k th to the last element and return the value
+  while (moves > 0) {
+    p = p->next;
+    moves--;
+  }
+  return p->data;
 }
 
-TEST_CASE("ch2.2 findKthToLast") {
+TEST_CASE("ch2.2 findKthToLast - [1,2,3,4,5]") {
   LinkedList list;
   list.insert(1);
   list.insert(2);
@@ -23,4 +43,12 @@ TEST_CASE("ch2.2 findKthToLast") {
   REQUIRE(findKthToLast(list, 2) == 3);
   REQUIRE(findKthToLast(list, 3) == 2);
   REQUIRE(findKthToLast(list, 4) == 1);
+  REQUIRE(findKthToLast(list, 5) == -1);
+  REQUIRE(findKthToLast(list, 1000) == -1);
+}
+
+TEST_CASE("ch2.2 findKthToLast - []") {
+  LinkedList list;
+
+  REQUIRE(findKthToLast(list, 0) == -1);
 }
--- cpp/src/ch2.3.cpp
+++ cpp/src/ch2.3.cpp
@@ -4,10 +4,27 @@
 #include "LinkedList.h"
 
 void deleteMiddle(const LinkedList& list) {
-  // TODO: implement
+  if (list.head == nullptr) {
+    return;
+  }
+
+  // Use a fast and a slow cursor, for every 1 move slow cursor makes, fast
+  // cursor will make 2 moves. When fast cursor gets to the end, slow cursor
+  // will be at the middle
+  Node* fast = list.head;
+  Node* slow = list.head;
+  while (fast->next != nullptr && fast->next->next != nullptr) {
+    fast = fast->next->next;
+    slow = slow->next;
+  }
+
+  Node* deleteNode = slow->next;
+  slow->data = slow->next->data;
+  slow->next = slow->next->next;
+  delete deleteNode;
 }
 
-TEST_CASE("ch2.3 deleteMiddle") {
+TEST_CASE("ch2.3 deleteMiddle - [1,2,3,4,5]") {
   LinkedList list;
   list.insert(1);
   list.insert(2);
@@ -15,6 +32,30 @@ TEST_CASE("ch2.3 deleteMiddle") {
   list.insert(4);
   list.insert(5);
   std::vector<int> expected = {1, 2, 4, 5};
+
+  deleteMiddle(list);
+  std::vector<int> actual = convertLinkedListToVector(list);
+
+  REQUIRE(expected == actual);
+}
+
+TEST_CASE("ch2.3 deleteMiddle - [1,2,3,4]") {
+  LinkedList list;
+  list.insert(1);
+  list.insert(2);
+  list.insert(3);
+  list.insert(4);
+  std::vector<int> expected = {1, 3, 4};
+
+  deleteMiddle(list);
+  std::vector<int> actual = convertLinkedListToVector(list);
+
+  REQUIRE(expected == actual);
+}
+
+TEST_CASE("ch2.3 deleteMiddle - []") {
+  LinkedList list;
+  std::vector<int> expected;
 
   deleteMiddle(list);
   std::vector<int> actual = convertLinkedListToVector(list);
