12517f92f2054a6e8d905b19d01ae5767f483b50
vince <vince.yp.chan@gmail.com>
2017-09-26 17:21:43 -0700

    feat: solution for ch2.7

--- cpp/src/ch2.7.cpp
+++ cpp/src/ch2.7.cpp
@@ -3,7 +3,30 @@
 #include "LinkedList.h"
 
 bool isPalindrome(const LinkedList& list) {
-  // TODO: implement
+  if (list.head == nullptr) {
+    return false;
+  }
+
+  // store a copy of the list in reverse order
+  LinkedList reverse;
+  Node* runner = list.head;
+  while (runner != nullptr) {
+    reverse.push_front(runner->value);
+    runner = runner->next;
+  }
+
+  // traverse both list, every item should have the same value if it's a
+  // palindrome
+  Node* first = list.head;
+  Node* second = reverse.head;
+  while (first != nullptr) {
+    if (first->value != second->value) {
+      return false;
+    }
+    first = first->next;
+    second = second->next;
+  }
+
   return true;
 }
 
