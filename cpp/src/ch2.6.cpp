5b776cb3d4d1fe035edc93928bc235b94ab6b1e8
Vince Chan <vince.yp.chan@gmail.com>
2017-10-07 01:59:09 -0700

    feat: ch2.6 solution

--- cpp/src/ch2.6.cpp
+++ cpp/src/ch2.6.cpp
@@ -9,20 +9,49 @@
  * Input: (A->B->C->D->E->C) the C is the same C as earlier
  * Ouput: C
  */
-int findLoop(const LinkedList& list) {
-  // TODO: implement
-  return 0;
+Node* findLoop(const LinkedList& list) {
+  if (list.head == nullptr) {
+    return nullptr;
+  }
+
+  // for every one move slow cursor makes, fast cursor will make 2 moves
+  Node* fast = list.head;
+  Node* slow = list.head;
+  while (fast->next != nullptr) {
+    fast = fast->next->next;
+    slow = slow->next;
+    if (fast == slow) {
+      break;
+    }
+  }
+
+  if (fast != slow) {
+    // no loop found
+    return nullptr;
+  }
+
+  // Loop found. Try to determine beginning of the loop.
+  // Point fast to head and leave slow at the last collision point.
+  // move both fast and slow one move at a time, when they meet again
+  // that's the beginning of the loop
+  fast = list.head;
+  while (fast != slow) {
+    fast = fast->next;
+    slow = slow->next;
+  }
+
+  return fast;
 }
 
 TEST_CASE("ch2.6 findLoop") {
-  LinkedList list(std::vector<int>({1, 2, 3, 4, 5}));
+  LinkedList list(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8}));
 
-  // create the loop so that node 5's next pointer points to node 3
+  // create the loop so that last node's next pointer points to node 4
   Node* last = list.head;
   while (last->next != nullptr) {
     last = last->next;
   }
-  last->next = list.head->next->next;
+  last->next = list.head->next->next->next;
 
-  REQUIRE(findLoop(list) == 3);
+  REQUIRE(findLoop(list)->value == 4);
 }
