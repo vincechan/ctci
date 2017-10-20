1302082add8bf4663c0be658d7c0bd0137122ab5
vince <vince.yp.chan@gmail.com>
2017-10-20 16:53:30 -0700

    feat: ch3.7 solution

--- cpp/src/ch3.7.cpp
+++ cpp/src/ch3.7.cpp
@@ -1,13 +1,90 @@
 #include "third_party/catch.hpp"
 
+#include <list>
+
 /**
  * Implement the sheleter class which operates on a FIFO basis. The built-in
  * linked list maybe used.
  */
-class Shelter {
- public:
-  void enqueue(int animalId);
-  int dequeueAny();
-  int dequeueDog();
-  int dequeueCat();
+enum AnimalType  {
+  Cat,
+  Dog
 };
+
+struct Animal {
+  AnimalType type;
+  int id;
+  Animal(AnimalType type, int id) : type(type), id(id) {}
+};
+
+class AnimalShelter {
+ public:
+  void enqueue(Animal animal) {
+    anyQ.push_back(animal);
+
+    if (animal.type == AnimalType::Cat) {
+      catQ.push_back(animal);
+    }
+    else {
+      dogQ.push_back(animal);
+    }
+  }
+
+  Animal dequeueAny() {
+    Animal animal = anyQ.front();
+    anyQ.pop_front();
+
+    if (animal.type == AnimalType::Cat) {
+      catQ.pop_front();
+    }
+    else {
+      dogQ.pop_front();
+    }
+    return animal;
+  }
+  
+  Animal dequeueDog() {
+    Animal animal = dogQ.front();
+    dogQ.pop_front();
+    anyQ.pop_front();
+    return animal;
+  }
+
+  Animal dequeueCat() {
+    Animal animal = catQ.front();
+    anyQ.pop_front();
+    catQ.pop_front();
+    return animal;
+  }
+
+ private:
+  std::list<Animal> anyQ;
+  std::list<Animal> dogQ;
+  std::list<Animal> catQ;
+};
+
+TEST_CASE("ch3.7 AnimalShelter - dequeueAny") {
+  AnimalShelter shelter;
+
+  shelter.enqueue(Animal(AnimalType::Cat, 1));
+  shelter.enqueue(Animal(AnimalType::Dog, 11));
+  shelter.enqueue(Animal(AnimalType::Cat, 2));
+  shelter.enqueue(Animal(AnimalType::Dog, 22));
+  REQUIRE(shelter.dequeueAny().id == 1);
+  REQUIRE(shelter.dequeueAny().id == 11);
+  REQUIRE(shelter.dequeueAny().id == 2);
+  REQUIRE(shelter.dequeueAny().id == 22);
+}
+
+TEST_CASE("ch3.7 AnimalShelter") {
+  AnimalShelter shelter;
+
+  shelter.enqueue(Animal(AnimalType::Cat, 1));
+  shelter.enqueue(Animal(AnimalType::Dog, 11));
+  shelter.enqueue(Animal(AnimalType::Cat, 2));
+  shelter.enqueue(Animal(AnimalType::Dog, 22));
+  REQUIRE(shelter.dequeueDog().id == 11);
+  REQUIRE(shelter.dequeueCat().id == 1);
+  REQUIRE(shelter.dequeueDog().id == 22);
+  REQUIRE(shelter.dequeueCat().id == 2);
+}
