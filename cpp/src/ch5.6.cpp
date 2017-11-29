3f0b12004fd15473b21709ac877ce01e9a346bd6
Vince Chan <vince.yp.chan@gmail.com>
2017-11-29 00:52:25 -0800

    ch5.6 solution

---
+++ cpp/src/ch5.6.cpp
@@ -0,0 +1,16 @@
+#include "third_party/catch.hpp"
+
+int SwapOddEvenBits(int n) {
+  int oddBits = n & 0xAAAAAAAA;   // 1010.....
+  int evenBits = n & 0x55555555;  // 0101.....
+  return (oddBits >> 1) | (evenBits << 1);
+}
+
+TEST_CASE("ch5.6 SwapOddEvenBits") {
+  // 0110 => 1001
+  REQUIRE(SwapOddEvenBits(6) == 9);
+
+  // 100100 => 011000
+  //     36 => 24
+  REQUIRE(SwapOddEvenBits(36) == 24);
+}
